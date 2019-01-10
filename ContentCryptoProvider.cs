2c847dc48891976855c0873987bb3d7a413bdbf3
Ogoun <ogoun@users.noreply.github.com>
2019-01-10 17:40:30 +0300

    First

---
+++ ContentCryptoProvider.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FastCypher
+{
+    internal class __FastCypher
+    {
+        private class Generator
+        {
+            #region Mask
+            List<byte> _maskTable = new List<byte>();
+            long generator;
+
+            private const int a = 43;
+            private const int m = 197;
+            #endregion
+
+            internal byte NextMask()
+            {
+                byte next = (byte)(generator % 255);
+                byte mask = 0;
+                for (short p = 0; p < 8; p++)
+                {
+                    mask |= (byte)(next & (2 << p));
+                }
+                generator = (a * generator + next) % m;
+                return mask;
+            }
+
+            internal Generator(long initial)
+            {
+                generator = initial;
+            }
+        }
+
+        private static Generator GetGenerator(string password)
+        {
+            if (string.IsNullOrEmpty(password) || password.Length < 2 || password.Length > 256)
+                throw new ArgumentOutOfRangeException("password", "Password length must be in range 2..256");
+            long initial = password[0];
+            for (int i = 1; i < Math.Min(password.Length, 15); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    initial += password[i] << i;
+                }
+                else
+                {
+                    initial ^= password[i] << i - 1;
+                }
+            }
+            return new Generator(initial);
+        }
+
+        internal static void Encode(Stream inputStream, Stream outputStream, string password)
+        {
+            var generator = GetGenerator(password);
+            var RRC = new RoundRobinCollection<byte>();
+            foreach (var b in Encoding.UTF8.GetBytes(password)) RRC.Add(b);
+            int count;
+            var buffer = new byte[8];
+            byte salt;
+            int position;
+            var currentSeq = RRC.GetCurrentSeq().GetEnumerator();
+            while ((count = inputStream.Read(buffer, 0, 8)) > 0)
+            {
+                salt = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    if (currentSeq.MoveNext() == false)
+                    {
+                        RRC.MoveNext();
+                        currentSeq = RRC.GetCurrentSeq().GetEnumerator();
+                        currentSeq.MoveNext();
+                    }
+                    position = currentSeq.Current % 8;
+                    buffer[i] ^= currentSeq.Current;
+                    buffer[i] ^= generator.NextMask();
+                    SetBit(i, ref salt, GetBit(position, buffer[i]));
+                    SetBit(position, ref buffer[i], GetBit(position, currentSeq.Current));
+                }
+                outputStream.Write(buffer, 0, count);
+                outputStream.WriteByte(salt);
+            }
+            RRC.Dispose();
+            RRC = null;
+        }
+
+        internal static void Decode(Stream inputStream, Stream outputStream, string password)
+        {
+            var generator = GetGenerator(password);
+            var RRC = new RoundRobinCollection<byte>();
+            foreach (var b in Encoding.UTF8.GetBytes(password)) RRC.Add(b);
+            int count;
+            var buffer = new byte[9];
+            byte salt;
+            int position;
+            var currentSeq = RRC.GetCurrentSeq().GetEnumerator();
+            while ((count = inputStream.Read(buffer, 0, 9)) > 0)
+            {
+                salt = buffer[count - 1];
+                for (int i = 0; i < count - 1; i++)
+                {
+                    if (currentSeq.MoveNext() == false)
+                    {
+                        RRC.MoveNext();
+                        currentSeq = RRC.GetCurrentSeq().GetEnumerator();
+                        currentSeq.MoveNext();
+                    }
+                    position = currentSeq.Current % 8;
+                    SetBit(position, ref buffer[i], GetBit(i, salt));
+                    buffer[i] ^= generator.NextMask();
+                    buffer[i] ^= currentSeq.Current;
+                }
+                outputStream.Write(buffer, 0, count - 1);
+            }
+            RRC.Dispose();
+            RRC = null;
+        }
+
+        private static bool GetBit(int position, byte num)
+        {
+            return (num & (1 << position)) != 0;
+        }
+
+        private static void SetBit(int position, ref byte num, bool value)
+        {
+            if (value)
+                num |= (byte)(1 << position); // 1
+            else
+                num &= (byte)(~(1 << position)); // 0
+        }
+
+        internal static byte InversionBit(byte original, params int[] positions)
+        {
+            byte result = original;
+            foreach (int position in positions)
+            {
+                result ^= (byte)(1 << position);
+            }
+            return result;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace FastCypher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string BaseDirectory =
+           System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly()?.CodeBase)?.
+           Replace("file:\\", string.Empty);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bEnc_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var problems = new StringBuilder();
+                var files = Directory.GetFiles(BaseDirectory, "*.*",
+                        (cbRecursive.IsChecked ?? false) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly).OrderBy(f => f).ToArray();
+                int proceed = files.Length;
+                ManualResetEvent completedEvent = new ManualResetEvent(false);
+                foreach (var _file in files)
+                {
+                    if (_file.IndexOf("FastCypher.exe", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        if (Interlocked.Decrement(ref proceed) == 0)
+                        {
+                            completedEvent.Set();
+                        }
+                        continue;
+                    }
+                    ThreadPool.QueueUserWorkItem(s =>
+                    {
+                        string file = (string)s;
+                        try
+                        {
+                            var currentDirectory = System.IO.Path.GetDirectoryName(file);
+                            var tempo = System.IO.Path.Combine(currentDirectory, System.IO.Path.GetFileName(file) + ".__tempo__");
+                            using (var input = new FileStream(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                            {
+                                using (var output = new FileStream(tempo, FileMode.Create, FileAccess.Write, FileShare.None))
+                                {
+                                    __FastCypher.Encode(input, output, pb.Password);
+                                    output.Flush();
+                                }
+                            }
+                            File.Delete(file);
+                            File.Move(tempo, file);
+                        }
+                        catch (Exception ex)
+                        {
+                            problems.AppendLine($"File {file} fault encoded. {ex.Message}");
+                        }
+                        finally
+                        {
+                            if (Interlocked.Decrement(ref proceed) == 0)
+                            {
+                                completedEvent.Set();
+                            }
+                        }
+                    }, _file);
+                }
+                completedEvent.WaitOne();
+                completedEvent.Close();
+                completedEvent = null;
+                MessageBox.Show("Completed.\r\n" + problems.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fault encode. " + ex.Message);
+            }
+        }
+
+        private void bDec_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var problems = new StringBuilder();
+                var files = Directory.GetFiles(BaseDirectory, "*.*",
+                        (cbRecursive.IsChecked ?? false) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly).OrderBy(f => f).ToArray();
+                int proceed = files.Length;
+                ManualResetEvent completedEvent = new ManualResetEvent(false);
+                foreach (var _file in files)
+                {
+                    if (_file.IndexOf("FastCypher.exe", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        if (Interlocked.Decrement(ref proceed) == 0)
+                        {
+                            completedEvent.Set();
+                        }
+                        continue;
+                    }
+                    ThreadPool.QueueUserWorkItem(s =>
+                    {
+                        string file = (string)s;
+                        try
+                        {
+                            var currentDirectory = System.IO.Path.GetDirectoryName(file);
+                            var tempo = System.IO.Path.Combine(currentDirectory, System.IO.Path.GetFileName(file) + ".__tempo__");
+                            using (var input = new FileStream(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                            {
+                                using (var output = new FileStream(tempo, FileMode.Create, FileAccess.Write, FileShare.None))
+                                {
+                                    __FastCypher.Decode(input, output, pb.Password);
+                                    output.Flush();
+                                }
+                            }
+                            File.Delete(file);
+                            File.Move(tempo, file);
+                        }
+                        catch (Exception ex)
+                        {
+                            problems.AppendLine($"File {file} fault decoded. {ex.Message}");
+                        }
+                        finally
+                        {
+                            if (Interlocked.Decrement(ref proceed) == 0)
+                            {
+                                completedEvent.Set();
+                            }
+                        }
+                    }, _file);
+                }
+                completedEvent.WaitOne();
+                completedEvent.Close();
+                completedEvent = null;
+                MessageBox.Show("Completed.\r\n" + problems.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fault decode. " + ex.Message);
+            }
+        }
+    }
+}
